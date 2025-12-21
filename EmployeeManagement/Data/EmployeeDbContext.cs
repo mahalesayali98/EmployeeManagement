cf61753280b393ee0d19c8c573494bd05a6dfe82
Sayali.Mahale <Sayali.Mahale@grdc.com.au>
2025-12-21 13:25:08 +0530

    EM-101 | Added Context

---
+++ EmployeeManagement/Data/EmployeeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using EmployeeManagement.Model;
+
+
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
